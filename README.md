8de9c9198f96c42d2f570b59b601151de140eee0
Simon Liu <simon.liu@jpl.nasa.gov>
2023-05-10 09:17:49 -0700

    add umm_t as options

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# cmr-publish-umms-action
+# cmr-publish-umm-action
 Github Action to publish UMM-S and UMM-T collection associations to CMR.
 
 ## Project Structure
 
-The UMM-S JSON file should be in a folder called `cmr` in the root
+The UMM JSON file should be in a folder called `cmr` in the root
 directory of the repo. Collection association files should be in the
 `cmr` folder and should be named `${env}_associations.txt` where
 `${env}` is the name of the environment (`uat`, `ops`, ...)
 
 ## Inputs
 
-### `umm-s-json`
+### `umm-json`
 
-**Required** The name of the JSON UMM-S file.
+**Required** The name of the JSON UMM file.
 
 ### `provider`
 
@@ -20,19 +20,22 @@ directory of the repo. Collection association files should be in the
 
 ### `env`
 
-**Required** The name of the environment to publish this UMM-S change
+**Required** The name of the environment to publish this UMM change
 to. Should be `uat`, `sit`, or `ops`
 
 ### `version`
 
 **Required** The version of the service. Will be substituted in to the
-UMM-S JSON before pushing to CMR.
+UMM JSON before pushing to CMR.
 
 ### `timeout`
 **Optional** API request timeout length in seconds.
 
 ### `disable_removal`
 Disable CMR association removal during sync event
+
+### `umm_type`
+Either update a umm-s or umm-t in umm defaults to umm-s
 
 ## Environment variables
 
@@ -67,14 +70,15 @@ is stored in a Github Secret.
 
 ```yaml
 - uses: actions/checkout@v2
-- name: UMM-S Updater Step
-  id: umm-s-updater
-  uses: podaac/cmr-umm-updater@0.0.1
+- name: UMM Updater Step
+  id: umm-updater
+  uses: podaac/cmr-umm-updater@0.0.4
   with:
-    umm-s-json: 'cmr/umm_s.json'
+    umm-s-json: 'cmr/umm.json'
     provider: 'POCLOUD'
     env: 'uat'
     version: '1.2.3'
+    umm_type: 'umm-s'
   env:
     cmr_user: ${{secrets.CMR_USER}}
     cmr_pass: ${{secrets.CMR_PASS}}
--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ if [[ $env == "sit" || $env == "uat" ]]; then
   jq --arg a "https://harmony.uat.earthdata.nasa.gov" '.URL.URLValue = $a' cmr/cmr.json > cmr/cmr.json.tmp && mv cmr/cmr.json.tmp cmr/cmr.json
 fi
 
-if [[ $disable_removal == "true" ]]; then
+if [[ $disable_removal == "true" && $umm_type == "umm-s" ]]; then
   if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
     umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout -r
   elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
@@ -28,7 +28,7 @@ if [[ $disable_removal == "true" ]]; then
   else
     umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout -r
   fi
-else
+elif [[ $disable_removal == "false" && $umm_type == "umm-s" ]]; then
   if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
     umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout
   elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
@@ -37,5 +37,25 @@ else
     umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout
   else
     umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout
+  fi
+elif [[ $disable_removal == "true" && $umm_type == "umm-t" ]]; then
+  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout -r
+  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout -r
+  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout -r
+  else
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout -r
+  fi
+elif [[ $disable_removal == "false" && $umm_type == "umm-t" ]]; then
+  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout
+  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout
+  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout
+  else
+    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout
   fi
 fi
