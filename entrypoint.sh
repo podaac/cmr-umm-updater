dd650048c3a223534f5199fa1beb1da34088223b
Simon Liu <simon.liu@jpl.nasa.gov>
2023-06-26 08:54:59 -0700

    update scripts to make associations file optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,57 +6,50 @@ file=$1
 provider=$2
 env=$3
 version=$4
-# ":-30" means If not set, use 30 as default
 timeout=${5:-30}
 disable_removal=$6
 umm_type=$7
+use_associations=${8:-false}
 
 # Replace version placeholder with actual version
-jq --arg a $version '.Version = $a' $file > "cmr/cmr.json"
+jq --arg a $version '.Version = $a' "$file" > "cmr/cmr.json"
 
 # Replace Harmony URL placeholder with Harmony URL based on env
 if [[ $env == "sit" || $env == "uat" ]]; then
   jq --arg a "https://harmony.uat.earthdata.nasa.gov" '.URL.URLValue = $a' cmr/cmr.json > cmr/cmr.json.tmp && mv cmr/cmr.json.tmp cmr/cmr.json
 fi
 
-if [[ $disable_removal == "true" && $umm_type == "umm-s" ]]; then
-  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout -r
-  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout -r
-  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout -r
+launchpad_token=""
+if [[ $env == "sit" && -n "${LAUNCHPAD_TOKEN_SIT}" ]]; then
+  launchpad_token=$LAUNCHPAD_TOKEN_SIT
+elif [[ $env == "uat" && -n "${LAUNCHPAD_TOKEN_UAT}" ]]; then
+  launchpad_token=$LAUNCHPAD_TOKEN_UAT
+elif [[ $env == "ops" && -n "${LAUNCHPAD_TOKEN_OPS}" ]]; then
+  launchpad_token=$LAUNCHPAD_TOKEN_OPS
+fi
+
+# Build the umms_updater/ummt_updater command
+command="umms_updater"
+if [[ $umm_type == "umm-t" ]]; then
+  command="ummt_updater"
+fi
+
+# Append additional options based on disable_removal value
+if [[ $disable_removal == "true" ]]; then
+  command+=" -r"
+fi
+
+# Execute the command
+if [[ -n $launchpad_token ]]; then
+  if [[ $use_associations == "true" ]]; then
+    "$command" -d -f "cmr/cmr.json" -a "cmr/${env}_associations.txt" -p "$provider" -e "$env" -t "$launchpad_token" -to "$timeout" -cu "$cmr_user" -cp "$cmr_pass"
   else
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout -r
+    "$command" -d -f "cmr/cmr.json" -p "$provider" -e "$env" -t "$launchpad_token" -to "$timeout" -cu "$cmr_user" -cp "$cmr_pass"
   fi
-elif [[ $disable_removal == "false" && $umm_type == "umm-s" ]]; then
-  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout
-  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout
-  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout
+else
+  if [[ $use_associations == "true" ]]; then
+    "$command" -d -f "cmr/cmr.json" -a "cmr/${env}_associations.txt" -p "$provider" -e "$env" -cu "$cmr_user" -cp "$cmr_pass" -to "$timeout"
   else
-    umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout
-  fi
-elif [[ $disable_removal == "true" && $umm_type == "umm-t" ]]; then
-  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout -r
-  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout -r
-  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout -r
-  else
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout -r
-  fi
-elif [[ $disable_removal == "false" && $umm_type == "umm-t" ]]; then
-  if [[ "${LAUNCHPAD_TOKEN_SIT}" ]] && [[ $env == "sit" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_SIT -to $timeout
-  elif [[ "${LAUNCHPAD_TOKEN_UAT}" ]] && [[ $env == "uat" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_UAT -to $timeout
-  elif [[ "${LAUNCHPAD_TOKEN_OPS}" ]] && [[ $env == "ops" ]]; then
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -t $LAUNCHPAD_TOKEN_OPS -to $timeout
-  else
-    ummt_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu $cmr_user -cp $cmr_pass -to $timeout
+    "$command" -d -f "cmr/cmr.json" -p "$provider" -e "$env" -cu "$cmr_user" -cp "$cmr_pass" -to "$timeout"
   fi
 fi
